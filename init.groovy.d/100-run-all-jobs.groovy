4ea50051bacd4fb2cfd0eb5cd7a5bf8abf74fca6
Mark Waite <mark.earl.waite@gmail.com>
2025-05-29 19:55:58 -0600

    Fix syntax error

--- init.groovy.d/100-run-all-jobs.groovy
+++ init.groovy.d/100-run-all-jobs.groovy
@@ -5,7 +5,7 @@ import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 
 Jenkins.instance.getAllItems(WorkflowJob.class).findAll().each {
-    if (it.fullName.contains("pipeline-all-start") {
+    if (it.fullName.contains("pipeline-all-start")) {
         continue
     }
     it.scheduleBuild(0, new UserIdCause())
