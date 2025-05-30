8ab175d50b084a3e4edd9e00cebe15c511436513
Mark Waite <mark.earl.waite@gmail.com>
2025-05-29 18:45:48 -0600

    Do not start pipeline-all-start on startup

--- init.groovy.d/100-run-all-jobs.groovy
+++ init.groovy.d/100-run-all-jobs.groovy
@@ -5,6 +5,9 @@ import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 
 Jenkins.instance.getAllItems(WorkflowJob.class).findAll().each {
+    if (it.fullName.contains("pipeline-all-start") {
+        continue
+    }
     it.scheduleBuild(0, new UserIdCause())
     println('Scheduled ' + it.fullName)
 }
